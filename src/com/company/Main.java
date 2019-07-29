81b0324e3e98c5875ab24a02b45bdef6f67fc514
jphomick <jphomick@gmail.com>
2019-07-29 16:39:25 -0400

    Compatibility with updated api

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,26 +5,50 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static long user;
+
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
         String command = "";
+        user = -1;
 
-
-            while (!command.equals("quit")) {
-                try {
+        while (!command.equals("quit")) {
+            try {
+                while (user < 0) {
+                    System.out.println("Type 'play [name]' to start a new game or 'load [id]' to continue");
                     command = read.nextLine();
-                    if (!(command.startsWith("check") || command.startsWith("reset")
-                            || command.startsWith("help")
-                            || command.startsWith("status") || command.startsWith("move"))) {
-                        command = "act_" + command;
-                    }
                     command = command.replace(" ", "_");
-                    String result = ReadHelper.readTextFromUrl("http://localhost:8080/" + command.trim());
-                    System.out.println(result.trim());
-                } catch (IOException e) {
-                    System.out.println("Invalid Read");
+                    if (command.startsWith("play")) {
+                        String result = ReadHelper.readTextFromUrl("http://localhost:8080/" + command.trim());
+                        System.out.println("Player created! Your id is '" + result + "'\nYou are in the Foyer.");
+                        user = Long.parseLong(result);
+                    } else if (command.startsWith("load") || command.startsWith("reset")) {
+                        String result = ReadHelper.readTextFromUrl("http://localhost:8080/" + command.trim());
+                        System.out.println(result.trim());
+                        if (command.startsWith("load")) {
+                            user = Long.parseLong(command.substring(5));
+                        }
+                    }
                 }
+                command = read.nextLine();
+                if (!(command.startsWith("check") || command.startsWith("reset")
+                        || command.startsWith("help")
+                        || command.startsWith("status") || command.startsWith("move"))) {
+                    command = "act_" + command;
+                }
+                if (!(command.startsWith("reset") || command.startsWith("help"))) {
+                    command = user + "_" + command;
+                }
+                command = command.replace(" ", "_");
+                String result = ReadHelper.readTextFromUrl("http://localhost:8080/" + command.trim());
+                System.out.println(result.trim());
+                if (command.equals("reset")) {
+                    user = -1;
+                }
+            } catch (IOException e) {
+                System.out.println("Invalid Read");
             }
+        }
 
     }
 }
